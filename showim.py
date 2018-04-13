1ba7730fb2077531175403ccf393b522fe161388
coderkill <rizvi1997@gmail.com>
2018-04-13 14:41:21 +0530

    Grayscale Working

--- showim.py
+++ showim.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import cv2
 image = cv2.imread("5_test.jpg")
-#im = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+im = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 im = cv2.resize(image, (28, 28))
+cv2.imwrite( "grey.jpg", im )
 
-plt.imshow(im)
+img=cv2.imread("grey.jpg")
+plt.imshow(img)
 plt.show()
